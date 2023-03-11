3dcd53e12bf059e4dafe52a4d0f87cbe9e73071c
David Meade Jr <davidmeadejnr@gmail.com>
2023-03-11 15:20:25 +0000

    Feat: Personal website created.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p className="headerName">David Meade Jr.</p>
+      <div className="aboutMe">
+        <ul>
+          <li>Full-stack software engineer focused on customer-centric products.</li>
+          <li>Experienced in Fintech, startups and web3.</li>
+          <li>
+            <a href="https://hackerthon.ai/">2022 Google Project Data Hack winner.</a>
+          </li>
+          <li>Now focused on creating value by building software.</li>
+          <li className="examples">
+            A few examples:
+            <ul>
+              <li>
+                <a href="https://www.collabz.online">Collabz</a>: A platform that connects small content creators with
+                brands and agencies.
+              </li>
+            </ul>
+          </li>
+          <li>
+            Previously, I was software engineer at Santander helping scale their in-house learning and mentoring
+            platform for software engineers from the ground up.
+          </li>
+          <li>I also was a software engineer at two VC-backed web3 startups focusing on front-end development.</li>
+          <li>
+            <a href="mailto:davidmeadejnr@gmail.com">Email</a>
+          </li>
+
+          <li>
+            <a href="https://www.linkedin.com/in/davidmeadejr/">LinkedIn</a>
+          </li>
+          <li>
+            <a href="https://github.com/davidmeadejr">GitHub</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/davidmeadejr">Twitter</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
-
-export default App;
