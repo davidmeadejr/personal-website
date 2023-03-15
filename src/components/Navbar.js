22ee16673d7c64625331941e48359ec94e3c297d
David Meade Jr <davidmeadejnr@gmail.com>
2023-03-15 08:43:09 +0000

    Fix: Removed emoji on title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
     <nav>
       <ul>
         <li className="navbarContainer typewriter">
-          <p className="title">David Meade Jr.👋🏾</p>
+          <p className="title">David Meade Jr.</p>
           <a href="#projects">Projects</a>
         </li>
       </ul>
