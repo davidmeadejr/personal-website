f08cef1003a37d2fca879c80c393d7b7f3e9dca0
David Meade Jr <davidmeadejnr@gmail.com>
2023-12-19 21:40:23 +0000

    Feat: Updated website

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const Home = () => {
         <h2>Work?</h2>
         <ul>
           <li>Programmer and ML Engineer</li>
-          <li>Interested in refining ML models for customer-centric products</li>
+          <li>Interested in modern computing and productising ML.</li>
           <li>
             Present: DevOps and SWE consultancy to UK public and private sector clients
           </li>
